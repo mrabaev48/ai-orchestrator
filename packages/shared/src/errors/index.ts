f49c604cc51991ec51708e1b0331bf96bb5ef2cf
msoosaar <mikhail.soosaar@xdatagroup.io>
2026-03-10 01:45:10 +0200

    feat(task-03): add orchestrator error hierarchy

---
+++ packages/shared/src/errors/index.ts
@@ -0,0 +1,135 @@
+export type ErrorCode =
+  | 'CONFIG_ERROR'
+  | 'STATE_STORE_ERROR'
+  | 'WORKFLOW_POLICY_ERROR'
+  | 'TOOL_EXECUTION_ERROR'
+  | 'LLM_PROVIDER_ERROR'
+  | 'SCHEMA_VALIDATION_ERROR'
+  | 'SAFETY_VIOLATION_ERROR'
+  | 'STATE_INTEGRITY_ERROR';
+
+export interface ErrorOptions {
+  cause?: unknown;
+  details?: unknown;
+  retrySuggested?: boolean;
+  needsHumanDecision?: boolean;
+  exitCode?: number;
+}
+
+export class OrchestratorError extends Error {
+  readonly code: ErrorCode;
+  readonly details?: unknown;
+  readonly retrySuggested: boolean;
+  readonly needsHumanDecision: boolean;
+  readonly exitCode: number;
+
+  constructor(code: ErrorCode, message: string, options: ErrorOptions = {}) {
+    super(message, { cause: options.cause });
+    this.name = new.target.name;
+    this.code = code;
+    this.details = options.details;
+    this.retrySuggested = options.retrySuggested ?? false;
+    this.needsHumanDecision = options.needsHumanDecision ?? false;
+    this.exitCode = options.exitCode ?? 1;
+  }
+
+  toJSON(): Record<string, unknown> {
+    return {
+      name: this.name,
+      code: this.code,
+      message: this.message,
+      retrySuggested: this.retrySuggested,
+      needsHumanDecision: this.needsHumanDecision,
+      exitCode: this.exitCode,
+      details: this.details,
+      cause: sanitizeCause(this.cause),
+    };
+  }
+}
+
+export class ConfigError extends OrchestratorError {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super('CONFIG_ERROR', message, { ...options, exitCode: 2 });
+  }
+}
+
+export class StateStoreError extends OrchestratorError {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super('STATE_STORE_ERROR', message, { ...options, exitCode: 3 });
+  }
+}
+
+export class WorkflowPolicyError extends OrchestratorError {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super('WORKFLOW_POLICY_ERROR', message, {
+      ...options,
+      exitCode: 4,
+      needsHumanDecision: options.needsHumanDecision ?? true,
+    });
+  }
+}
+
+export class ToolExecutionError extends OrchestratorError {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super('TOOL_EXECUTION_ERROR', message, {
+      ...options,
+      retrySuggested: options.retrySuggested ?? true,
+      exitCode: 5,
+    });
+  }
+}
+
+export class LlmProviderError extends OrchestratorError {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super('LLM_PROVIDER_ERROR', message, {
+      ...options,
+      retrySuggested: options.retrySuggested ?? true,
+      exitCode: 6,
+    });
+  }
+}
+
+export class SchemaValidationError extends OrchestratorError {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super('SCHEMA_VALIDATION_ERROR', message, {
+      ...options,
+      retrySuggested: options.retrySuggested ?? true,
+      exitCode: 7,
+    });
+  }
+}
+
+export class SafetyViolationError extends OrchestratorError {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super('SAFETY_VIOLATION_ERROR', message, {
+      ...options,
+      exitCode: 8,
+      needsHumanDecision: options.needsHumanDecision ?? true,
+    });
+  }
+}
+
+export class StateIntegrityError extends OrchestratorError {
+  constructor(message: string, options: ErrorOptions = {}) {
+    super('STATE_INTEGRITY_ERROR', message, {
+      ...options,
+      exitCode: 9,
+      needsHumanDecision: options.needsHumanDecision ?? true,
+    });
+  }
+}
+
+export function isRetryable(error: unknown): boolean {
+  return error instanceof OrchestratorError && error.retrySuggested;
+}
+
+function sanitizeCause(cause: unknown): unknown {
+  if (cause instanceof Error) {
+    return {
+      name: cause.name,
+      message: cause.message,
+    };
+  }
+
+  return cause;
+}
